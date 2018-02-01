95ff39df668461bb274ce3943dd8fa5f3bd356c9
Lawrence <lgwong168@gmail.com>
2018-02-01 12:11:03 -0800

    Create geo.cpp

---
+++ hw1/geo.cpp
@@ -0,0 +1,38 @@
+/*
+@author: Lawrence Wong
+@version: 2/1/18
+*/
+
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int geo(int n, int a)
+{
+    int sum = 0;
+    for (int i = 1; i <= n; i++)
+    {
+        int j = 1;
+        for (int k = 0; k < i; k++)
+        {
+            j *= a;
+        }
+        sum += j;
+    }
+    return sum;
+}
+
+int main()
+{
+	// ofstream fout("geo.csv");
+
+	cout << "n\ta\tsum" << "\n";
+	for (int i = 1; i <= 5; i++)
+	{
+	    for (int j = 1; j <= 5; j++)
+    		cout << i << "\t" << j << "\t" << geo(i, j) << "\n";
+	}
+
+	return 0;
+}
